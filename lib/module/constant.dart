a42d0f742c47c3a22746d06c3681f343bd82d913
ai220072 <ai220072@student.uthm.edu.my>
2025-06-19 14:17:36 +0800

    Update constant.dart

--- lib/module/constant.dart
+++ lib/module/constant.dart
@@ -1,2 +1,2 @@
 const String apiSecretKey =
-    "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";
+    "YOUR API KEY HERE"; //You API Key
